dcd913e8b86626885dd26308e28ed5411d4b8865
Tim Hhh <timhon+st@gmail.com>
2016-12-11 23:20:03 -0800

    test

---
+++ smartapps/timh5/test1.src/test1.groovy
@@ -0,0 +1,42 @@
+/**
+ *  test1
+ *
+ *  Copyright 2016 Tim Hhh
+ *
+ */
+definition(
+    name: "test1",
+    namespace: "timh5",
+    author: "Tim Hhh",
+    description: "abctest",
+    category: "",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: [displayName: "dispNAme", displayLink: ""])
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
